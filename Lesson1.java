9a4fbb1830c7571c3fc2ec7ebff34d507843c0ff
uni51 <uni51@users.noreply.github.com>
2018-05-02 18:37:33 +0900

    添削結果反映

--- Lesson1.java
+++ Lesson1.java
@@ -1,23 +1,41 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+//●解答サンプル：パターン１（Stringクラスのまま比較）
+//import java.io.*;
+//public class Lesson1 {
+//	public static void main(String args[]) throws IOException{
+//		System.out.print("数値を入力して下さい。" );
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		String str = br.readLine();
+//		switch (str){
+//			case "1":
+//			case "2":
+//			case "3":
+//				System.out.println(str + "が入力されました。");
+//				break;
+//			default:
+//				System.out.println("1～3以外の数字が入力されました。");
+//		}
+//	}
+//}
+
+// ●解答サンプル：パターン２（int型に変換後比較）
 public class Lesson1 {
-	public static void main (String args[]) throws IOException {
-		System.out.println("数字を入力して下さい。");
+	public static void main(String args[]) throws IOException{
+		System.out.print("数値を入力して下さい。" );
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = br.readLine(); // コンソールに入力した内容を文字列として取得する
-		char res = str.charAt(0);
-		switch(res){
-		case '1':
-			System.out.println("1が入力されました。");
-			break;
-		case '2':
-			System.out.println("2が入力されました。");
-			break;
-		case '3':
-			System.out.println("3が入力されました。");
-			break;
-		default:	// 上記以外の場合
-			System.out.println("1～3以外の数字が入力されました。");
+		String str = br.readLine();
+		int res = Integer.parseInt(str);
+		switch (res){
+			case 1:
+			case 2:
+			case 3:
+				System.out.println(str + "が入力されました。");
+				break;
+			default:
+				System.out.println("1～3以外の数字が入力されました。");
 		}
 	}
 }
--- Lesson3.java
+++ Lesson3.java
@@ -1,4 +1,6 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Lesson3 {
 	public static void main (String args[]) throws IOException {
@@ -11,13 +13,37 @@ public class Lesson3 {
 		BufferedReader br2 = new BufferedReader(new InputStreamReader(System.in));
 		String str2 = br2.readLine(); // コンソールに入力した内容を文字列として取得する
 		int y = Integer.parseInt(str2);
-		
+
 		if(x==y){
 			System.out.println("2つの数は同じ値です。");
 		}else if(x>y){
 			System.out.println("XよりYの方が小さい値です。");
 		}else{
-			System.out.println("XよりYの方が大きい値です。");			
+			System.out.println("XよりYの方が大きい値です。");
 		}
 	}
 }
+
+//★結果表示については、以下のように記述する事も出来ますので参考にして下さい。
+/*
+import java.io.*;
+public class Lesson3 {
+
+	public static void main(String args[]) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("数値1を入力して下さい。" );
+		String str1 = br.readLine();
+		System.out.print("数値2を入力して下さい。" );
+		String str2 = br.readLine();
+		int res1 = Integer.parseInt(str1);//br.readLine()で取得した文字列（str1）を数値に変換
+		int res2 = Integer.parseInt(str2);//br.readLine()で取得した文字列（str2）を数値に変換
+		if (res1==res2){
+			System.out.println("2つの数は同じ値です。");
+		}else if(res1<res2){
+			System.out.println(res1 +"より" + res2 + "の方が大きい値です。");
+		}else if(res1>res2){
+			System.out.println(res1 +"より" + res2 + "の方が小さい値です。");
+		}
+	}
+}
+*/
